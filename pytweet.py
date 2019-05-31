581edd035b2274cef8d3be9ec64417b080153603
harbhajan.rai <harbhajan.rai@veritas.com>
2019-05-31 17:31:10 +0530

    trends by country

--- pytweet.py
+++ pytweet.py
@@ -1,4 +1,7 @@
 import twitter
+import yweather
+
+from pprint import pprint
 
 api = twitter.Api(
     consumer_key="<consumer-key>",
@@ -8,9 +11,32 @@ api = twitter.Api(
 )
 
 
-print("Trending #hashtags across the world")
+def get_where_on_earth_id(country = None):
+    if country:
+        client = yweather.Client()
+        woeid = client.fetch_woeid(country)
+        return woeid
 
-trends = api.GetTrendsCurrent()
 
-for trend in trends:
-    print(trend.name)
+def get_trends(country = None):
+    woeid = get_where_on_earth_id(country)
+    print(woeid)
+    if country :
+        try:
+            trends = api.GetTrendsWoeid(woeid, exclude=None)
+            pprint(trends)
+        except Exception as e:
+            print(e[0][0]["message"])
+    else:
+        try:
+            trends = api.GetTrendsCurrent()
+            pprint(trends)
+        except Exception as e:
+            print(e[0][0]["message"])
+
+
+if __name__ == "__main__":
+    # edit this according to country of your choice
+    country_name = None
+    get_trends(country_name)
+
