411e4ca3cb90710feca18a387f82d0a3f4e6392f
harbhajan.rai <kishanrai99999@gmail.com>
2019-09-04 10:44:09 +0530

    adding code to get friends

---
+++ friends.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# twitter-friends
+#  - lists all of a given user's friends (ie, followees)
+#-----------------------------------------------------------------------
+
+from twitter import *
+
+#-----------------------------------------------------------------------
+# load our API credentials
+#-----------------------------------------------------------------------
+import sys
+sys.path.append(".")
+import config
+
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# create twitter API object
+twitter = Twitter(
+    auth = OAuth(
+        config.access_key,
+        config.access_secret,
+        config.consumer_key,
+        config.consumer_secret
+    )
+)
+
+# the user whose friends will  be listed
+print("   Username whose friends need to be listed:", end=' ')
+username = input()
+
+#-----------------------------------------------------------------------
+# perform a basic search
+# twitter API docs: https://dev.twitter.com/rest/reference/get/friends/ids
+#-----------------------------------------------------------------------
+query = twitter.friends.ids(screen_name = username)
+
+#-----------------------------------------------------------------------
+# tell the user how many friends we've found.
+# note that the twitter API will NOT immediately give us any more
+# information about friends except their numeric IDs...
+#-----------------------------------------------------------------------
+print("Found %d friends\n" % (len(query["ids"])))
+
+#-----------------------------------------------------------------------
+# now we loop through them to pull out more info, in blocks of 100.
+#-----------------------------------------------------------------------
+for n in range(0, len(query["ids"]), 100):
+    ids = query["ids"][n:n+100]
+
+    #-----------------------------------------------------------------------
+    # create a comma-separated string from the ID list
+    #-----------------------------------------------------------------------
+    ids_string = ",".join(str(id) for id in ids)
+
+    #-----------------------------------------------------------------------
+    # create a subquery, looking up information about these users
+    # twitter API docs: https://dev.twitter.com/rest/reference/get/users/lookup
+    #-----------------------------------------------------------------------
+    subquery = twitter.users.lookup(user_id = ids_string)
+
+    for user in subquery:
+        #-----------------------------------------------------------------------
+        # now print out user info, starring any users that are Verified.
+        #-----------------------------------------------------------------------
+        print(" [%s] %s - %s" % ("*" if user["verified"] else " ", user["screen_name"], user["location"]))
